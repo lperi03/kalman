f4493af2336d2930e6176ea18296ae371aeae032
lperi03 <lperi@berkeley.edu>
2022-12-23 17:40:40 +0530

    create kalman.cpp
    Initial commit, added descriptions

---
+++ kalman.cpp
@@ -0,0 +1,9 @@
+// my implementation of a kalman filter
+// kalman filter is a very useful recursive filtering algorithm often used for state estimation. This file will walk you through the initialization of a kalman filter in C++
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+class KalmanFilter {
+    
+}
