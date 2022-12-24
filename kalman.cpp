940116d5513d0e6a069f9fa5c9c2a8cb91579e2c
lperi03 <lperi@berkeley.edu>
2022-12-24 10:59:26 +0530

    updates
    added some definitions, class constructor

--- kalman.cpp
+++ kalman.cpp
@@ -1,9 +1,16 @@
 // my implementation of a kalman filter
 // kalman filter is a very useful recursive filtering algorithm often used for state estimation. This file will walk you through the initialization of a kalman filter in C++
+// kalman filter is used in linear systems to estimate states using the equation: xk = F(xk-1) + B(uk-1) + wk-1, where k and k -1 are the current and previpus timestep respectively
+// F is the state transition matrix, xk - 1 is the previous state vector, B is the control input matrix applied to control vector uk-1, and wk - 1 is process noise vector.
+
 #include <iostream>
 #include <vector>
 #include <cmath>
 
 class KalmanFilter {
+    // constructor for the filter
+    public:
+        KalmanFilter(double initial_value, double process_noise, )
+
     
 }
